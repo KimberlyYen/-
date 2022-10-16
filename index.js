8e7edab6c9520f54f112bcd680686aa95c247317
Kimberly <work223305@gmail.com>
2022-10-16 20:57:42 +0800

    finish

--- index.js
+++ index.js
@@ -1,55 +1,131 @@
 const players = [
-  { name: "Bernard", email: "bernard@example.com", ticket: "XL3558" },
-  { name: "Youchi", email: "youchi@example.com", ticket: "AH9188" },
-  { name: "Yenting", email: "yenting@example.com", ticket: "LO9903" },
-  { name: "Angela", email: "angela@example.com", ticket: "HY7212" },
-  { name: "Yvonne", email: "yvonne@example.com", ticket: "CH7684" },
-  { name: "Ellen", email: "ellen@example.com", ticket: "BB1750" },
-  { name: "Walter", email: "walter@example.com", ticket: "EI5724" },
-  { name: "Kevin", email: "kevin@example.com", ticket: "TT1804" },
-  { name: "Tim", email: "tim@example.com", ticket: "CK4592" },
-  { name: "Russell", email: "russell@example.com", ticket: "SI0305" },
+  { name: "Bernard", email: "bernard@example.com" },
+  { name: "Youchi", email: "youchi@example.com" },
+  { name: "Yenting", email: "yenting@example.com" },
+  { name: "Angela", email: "angela@example.com" },
+  { name: "Yvonne", email: "yvonne@example.com" },
+  { name: "Ellen", email: "ellen@example.com" },
+  { name: "Walter", email: "walter@example.com" },
+  { name: "Kevin", email: "kevin@example.com" },
+  { name: "Tim", email: "tim@example.com" },
+  { name: "Russell", email: "russell@example.com" },
 ];
 
+const tickets = [];
+// 抽取得獎者
 function drawWinner(players, prize) {
-  //1. 所有的參賽者，任選一位(座號索引)
-  let pickOne = Math.floor(Math.random() * players.length);
-
-  //2. 由座號索引，叫出對應名單
+  // random 0 ~ 0.9999
+  // floor 去掉小數點
+  const randomIndex = Math.floor(Math.random() * players.length);
+  const player = players.splice(randomIndex, 1);
+  announceMsg(players[randomIndex], prize);
+}
+// 宣布得獎者
+function announceMsg(winner, prize) {
   console.log(
-    `${announce(players[pickOne].name)} | ${players[pickOne].ticket} | ${secret(
-      players[pickOne].email
+    `${winner.number} | ${encodeName(winner.name)} | ${encodeEmail(
+      winner.email
     )} | ${prize} `
   );
 }
 
-// Q2: 加密姓名
-function announce(input) {
-  let anyString = input;
-  let anyString4 = anyString.substring(anyString.length - anyString.length / 2);
+// 加密姓名
+function encodeName(name) {
+  let eName = "";
+  const first = name[0];
+  const second = name[1];
+  const star = name.length - 2;
 
-  const who = anyString.replace(anyString4, "****");
-  return who;
+  eName = eName + first + second;
+
+  for (let i = 0; i < star; i++) {
+    eName = eName + "*";
+  }
+
+  return eName;
 }
 
-// Q3: 加密信箱
-function secret(input) {
-  // @ 的前面
-  const emailCut = input.split("@");
+function encodeEmail(email) {
+  let eEmail = "";
+  // 從 @ 切割
+  const cutEmailByAt = email.split("@");
+  // 把 @ 之前 / 2
+  const emailLength = Math.floor(cutEmailByAt[0].length / 2);
+  // 每一個都做一遍
+  for (let i = 0; i < emailLength; i++) {
+    // 組合新的 email
+    // 新的 email 會是一半的前方
+    eEmail = eEmail + email[i];
+  }
 
-  // @ 的前面的一半
-  let howMany = Math.ceil(emailCut[0].length / 2);
-
-  // 字串倒著數一半，做取代
-  let replaceEmail = emailCut[0].slice(-howMany);
-  let result = input.replace(replaceEmail, "...");
-
-  return result;
+  eEmail = eEmail + "...@" + cutEmailByAt[1];
+  return eEmail;
 }
 
-// 貳獎 推進陣列
-// 參獎 推進陣列
-// 剩下 推進陣列
+//製作英文
+function randomCapital() {
+  const Capital = "ABCDEFGHIGKLMNOPQRSTUVWXYZ";
+  const randonCapitalIndex = Math.floor(Math.random() * Capital.length);
+  const randomCapital = Capital[randonCapitalIndex];
+  return randomCapital;
+}
+
+function getTicket() {
+  let ticketNumber = " ";
+  //製作英文
+  const capitals = randomCapital() + randomCapital();
+  //製作數字
+  let number =
+    Math.floor(Math.random() * 10).toString() +
+    Math.floor(Math.random() * 10).toString() +
+    Math.floor(Math.random() * 10).toString() +
+    Math.floor(Math.random() * 10).toString();
+
+  ticketNumber = capitals + number;
+
+  // 抽獎號碼不重複 indexOf 有存在會在 0 ~ n , 沒有則返回 -1
+  if (tickets.indexOf(ticketNumber) >= 0) {
+    getTicket();
+  } else {
+    tickets.push(ticketNumber);
+    return ticketNumber;
+  }
+
+  return ticketNumber;
+}
+
+for (let i = 0; i < players.length; i++) {
+  players[i]["number"] = getTicket();
+}
+
 drawWinner(players, "頭獎");
 drawWinner(players, "貮獎");
 drawWinner(players, "叁獎");
+
+for (let i = 0; i < players.length; i++) {
+  announceMsg(players[i], "參加獎");
+}
+
+// // Q2: 加密姓名
+// function announce(input) {
+//   let anyString = input;
+//   let anyString4 = anyString.substring(anyString.length - anyString.length / 2);
+
+//   const who = anyString.replace(anyString4, "****");
+//   return who;
+// }
+
+// // Q3: 加密信箱
+// function secret(input) {
+//   // @ 的前面
+//   const emailCut = input.split("@");
+
+//   // @ 的前面的一半
+//   let howMany = Math.ceil(emailCut[0].length / 2);
+
+//   // 字串倒著數一半，做取代
+//   let replaceEmail = emailCut[0].slice(-howMany);
+//   let result = input.replace(replaceEmail, "...");
+
+//   return result;
+// }
